7a34ff12b36585b4d7a5069c67829ec602e4d872
icmoore <ic3moore@gmail.com>
2022-04-26 14:39:14 -0700

    task 104 complete

--- python/dev/token/.ipynb_checkpoints/RebaseToken-checkpoint.py
+++ python/dev/token/.ipynb_checkpoints/RebaseToken-checkpoint.py
@@ -8,7 +8,7 @@ class RebaseToken(Token):
     
     def __init__(self, name, supply = None, addresses = None):
         super().__init__(name, None, None)
-        self.__state = None
+        self.__ini_state = None
         self.__state_map = MapStateSeries(name)
  
     def get_state_map(self):
@@ -18,8 +18,8 @@ class RebaseToken(Token):
         return self.__state_map.get_states(address)
     
     def init_token(self, time0, address):
-        self.__state = State(Deposit(0,0,0))
-        self.__state.init_first_state(time0)
+        self.__ini_state = State(Deposit(0,0,0))
+        self.__ini_state.init_first_state(time0)
  
     def add_event(self, event):
         
@@ -27,17 +27,20 @@ class RebaseToken(Token):
         supply = super().get_supply()
         address = event.get_address()
         delta = event.get_delta()
-        
-        self.__state.update_event(event)
-        delta = delta+self.__state.get_yield() 
-        
+          
         if(not addresses.address_exist(address)):
             self.__state_map.add_state_series(StateSeries(), address)
+            state = self.__ini_state
+            state.update_event(event)
+            delta = delta+state.get_yield()                  
             addresses.set_balance(delta, address)
-        else:        
+        else:     
+            state = self.__state_map.get_states(address).get_last_state()
+            state.update_event(event)
+            delta = delta+state.get_yield()             
             addresses.delta_balance(delta, address)
             
-        self.__state_map.add_state(self.__state, address)                 
+        self.__state_map.add_state(state, address)                 
         supply.rebase(delta)  
                 
         
