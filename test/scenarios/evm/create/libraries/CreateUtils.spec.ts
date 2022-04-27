870f827e6e56a975397c8b9b5381e661f06662c4
cyotee doge <cyotee@unstoppable.email>
2022-04-27 11:19:08 -0700

    Updated Create2Utils test

--- test/scenarios/evm/create/libraries/CreateUtils.spec.ts
+++ test/scenarios/evm/create/libraries/CreateUtils.spec.ts
@@ -8,28 +8,28 @@ import {
 
 describe('Create2Utils', function () {
   
-  let instance: CreateUtilsMock;
+  let createUtils: CreateUtilsMock;
 
   beforeEach(async function () {
     const [deployer] = await ethers.getSigners();
-    instance = await new CreateUtilsMock__factory(deployer).deploy();
+    createUtils = await new CreateUtilsMock__factory(deployer).deploy();
   });
 
   // describeBehaviorOfFactory({ deploy: async () => instance });
 
-  describe('__internal', function () {
+  describe('internal', function () {
     describe('#_deploy', function () {
       describe('(bytes)', function () {
         it('deploys bytecode and returns deployment address', async function () {
-          const initCode = instance.deployTransaction.data;
+          const initCode = createUtils.deployTransaction.data;
 
-          const address = await instance.callStatic['deploy(bytes)'](initCode);
+          const address = await createUtils.callStatic.deploy(initCode);
           expect(address).to.be.properAddress;
 
-          await instance['deploy(bytes)'](initCode);
+          await createUtils.deploy(initCode);
 
           expect(await ethers.provider.getCode(address)).to.equal(
-            await ethers.provider.getCode(instance.address),
+            await ethers.provider.getCode(createUtils.address),
           );
         });
 
@@ -37,7 +37,7 @@ describe('Create2Utils', function () {
           it('contract creation fails', async function () {
             const initCode = '0xfe';
 
-            await expect(instance['deploy(bytes)'](initCode)).to.revertedWith(
+            await expect(createUtils.deploy(initCode)).to.revertedWith(
               'CreateUtils: failed deployment',
             );
           });
