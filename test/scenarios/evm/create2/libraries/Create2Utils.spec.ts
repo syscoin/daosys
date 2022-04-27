609854ac2a0792d44753170b2b5133c5d58dcc78
cyotee doge <cyotee@unstoppable.email>
2022-04-27 11:19:57 -0700

    Updated Create2Utils test

--- test/scenarios/evm/create2/libraries/Create2Utils.spec.ts
+++ test/scenarios/evm/create2/libraries/Create2Utils.spec.ts
@@ -8,40 +8,37 @@ import {
 
 describe('Factory', function () {
   
-  let instance: Create2UtilsMock;
+  let create2Utils: Create2UtilsMock;
 
   beforeEach(async function () {
     const [deployer] = await ethers.getSigners();
-    instance = await new Create2UtilsMock__factory(deployer).deploy();
+    create2Utils = await new Create2UtilsMock__factory(deployer).deploy();
   });
 
-  // describeBehaviorOfFactory({ deploy: async () => instance });
+  // describeBehaviorOfFactory({ deploy: async () => create2Utils });
 
   describe('__internal', function () {
     describe('#_deployWithSalt', function () {
 
       describe('(bytes,bytes32)', function () {
         it('deploys bytecode and returns deployment address', async function () {
-          const initCode = await instance.deployTransaction.data;
+          const initCode = await create2Utils.deployTransaction.data;
           const initCodeHash = ethers.utils.keccak256(initCode);
           const salt = ethers.utils.randomBytes(32);
 
-          const address = await instance.callStatic['deployWithSalt(bytes,bytes32)'](
-            initCode,
-            salt,
-          );
+          const address = await create2Utils.callStatic.deployWithSalt(initCode, salt);
           expect(address).to.equal(
-            await instance.callStatic.calculateDeploymentAddress(
-              instance.address,
+            await create2Utils.callStatic.calculateDeploymentAddress(
+              create2Utils.address,
               initCodeHash,
               salt,
             ),
           );
 
-          await instance['deployWithSalt(bytes,bytes32)'](initCode, salt);
+          await create2Utils.deployWithSalt(initCode, salt);
 
           expect(await ethers.provider.getCode(address)).to.equal(
-            await ethers.provider.getCode(instance.address),
+            await ethers.provider.getCode(create2Utils.address),
           );
         });
 
@@ -51,18 +48,18 @@ describe('Factory', function () {
             const salt = ethers.utils.randomBytes(32);
 
             await expect(
-              instance['deployWithSalt(bytes,bytes32)'](initCode, salt),
+              create2Utils.deployWithSalt(initCode, salt),
             ).to.revertedWith('Create2Utils: failed deployment');
           });
 
           it('salt has already been used', async function () {
-            const initCode = instance.deployTransaction.data;
+            const initCode = create2Utils.deployTransaction.data;
             const salt = ethers.utils.randomBytes(32);
 
-            await instance['deployWithSalt(bytes,bytes32)'](initCode, salt);
+            await create2Utils.deployWithSalt(initCode, salt);
 
             await expect(
-              instance['deployWithSalt(bytes,bytes32)'](initCode, salt),
+              create2Utils.deployWithSalt(initCode, salt),
             ).to.be.revertedWith('Create2Utils: failed deployment');
           });
         });
@@ -71,18 +68,18 @@ describe('Factory', function () {
 
     describe('#_calculateDeploymentAddress', function () {
       it('returns address of not-yet-deployed contract', async function () {
-        const initCode = instance.deployTransaction.data;
+        const initCode = create2Utils.deployTransaction.data;
         const initCodeHash = ethers.utils.keccak256(initCode);
         const salt = ethers.utils.randomBytes(32);
 
         expect(
-          await instance.callStatic.calculateDeploymentAddress(
-            instance.address,
+          await create2Utils.callStatic.calculateDeploymentAddress(
+            create2Utils.address,
             initCodeHash,
             salt
           ),
         ).to.equal(
-          ethers.utils.getCreate2Address(instance.address, salt, initCodeHash),
+          ethers.utils.getCreate2Address(create2Utils.address, salt, initCodeHash),
         );
       });
     });
