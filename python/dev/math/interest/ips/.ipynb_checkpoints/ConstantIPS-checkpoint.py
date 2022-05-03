9ee6d681ddbd75dc7f1ce0bd3030ad04d28f3efd
icmoore <ic3moore@gmail.com>
2022-05-03 10:05:32 -0700

    task 103 complete

--- python/dev/math/interest/ips/.ipynb_checkpoints/ConstantIPS-checkpoint.py
+++ python/dev/math/interest/ips/.ipynb_checkpoints/ConstantIPS-checkpoint.py
@@ -1,11 +1,24 @@
 from python.dev.math.interest.ips import IPS
 
 class ConstantIPS(IPS):
+       
+    def __swap_balance(self, balance, price):
         
-    def calc_ips_from_state(self, state):
-        A0 = state.get_principle() - state.get_delta() 
-        A1 = state.get_balance() - state.get_delta() 
+        if price == None:
+            return balance
+        else:
+            return balance*price
+        
+    def calc_ips_from_state(self, state, price = None):
+              
+        A0 = state.get_balance() - state.get_yield() - state.get_delta()
+        A0 = self.__swap_balance(A0, price)
+        
         dt = state.get_event().get_time_delta()
+        
+        A1 = A0 + state.get_yield()
+        A1 = self.__swap_balance(A1, price)
+        
         return self.calc_ips(A0, A1, dt)
     
     def calc_ips(self, a0, a1, dt):
