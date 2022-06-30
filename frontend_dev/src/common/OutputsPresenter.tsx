24fb879ef2ca0b1c5246908dcbb2e53737ddf1ea
Eskrano <pommmper@gmail.com>
2022-06-30 20:17:29 +0300

    Outputs

--- frontend_dev/src/common/OutputsPresenter.tsx
+++ frontend_dev/src/common/OutputsPresenter.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from "react";
 import { Falsy } from "@usedapp/core/dist/esm/src/model/types";
+import { ethers } from "ethers"
 
 export interface OutputsPresenterProps {
     error: boolean,
@@ -11,22 +12,33 @@ export const OutputsPresenter: FC<OutputsPresenterProps> = (props) => {
 
     if (props.error) {
         return (<>
-            <div className = "alert alert-danger">
+            <div className="alert alert-danger">
                 <p>
-                    {JSON.stringify(props.results, null , 4)}
-                </p>    
+                    {JSON.stringify(props.results, null, 4)}
+                </p>
             </div>
         </>)
     }
 
     return (<>
-        <div className="alert alert-primary">
-            
-            {(typeof props.results !== 'undefined' && props.results.length > 0) && Object.keys(props.results).map( (item , index )=> {
-                return <>
-                    # {index} - {item}
-                </>
-            })}
-        </div>
+        {(typeof props.results !== 'undefined' && props.results.length > 0) && <>
+            <div className="alert alert-primary">
+                <ul>
+                    {Object.keys(props.results).map((item, index) => {
+
+                        let value = props.results[item];
+
+                        if (typeof value === 'object' && '_isBigNumber' in value) {
+                            value = ethers.utils.formatUnits(value).toString()
+                        }
+
+                        return <li key={index}>
+                            {value}
+                        </li>
+                    })}
+                </ul>
+            </div>
+        </>}
+
     </>)
 }
