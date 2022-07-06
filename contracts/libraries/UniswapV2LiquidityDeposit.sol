94ff0ffc73afc0758d4cf4a19d552a9ffa51fc9d
icmoore <ic3moore@gmail.com>
2022-07-06 15:55:43 -0700

    update

---
+++ contracts/libraries/UniswapV2LiquidityDeposit.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+pragma solidity ^0.8.0;
+
+import "../test/protocols/dexes/uniswap/v2/uniswap-v2-core/libraries/Math.sol";
+import "../test/protocols/dexes/uniswap/v2/uniswap-v2-core/interfaces/IUniswapV2ERC20.sol";
+import "../test/protocols/dexes/uniswap/v2/uniswap-v2-core/interfaces/IUniswapV2Pair.sol";
+import "../test/protocols/dexes/uniswap/v2/uniswap-v2-periphery/libraries/UniswapV2Library.sol";
+import "../tokens/erc20/interfaces/IERC20.sol";
+
+import "hardhat/console.sol";
+
+library UniswapLiquidityDeposit {
+    using SafeMath for uint;
+
+    function testFunc(
+        uint x
+    ) internal pure returns (
+        uint y
+    ) {
+        y = 10000000000000000000;
+    }
+
+
+}    
